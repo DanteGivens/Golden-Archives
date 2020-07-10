cf042664e354780aeaa576723a44288410d2bfb4
DanteGivens <67976030+DanteGivens@users.noreply.github.com>
2020-07-09 20:46:58 -0400

    Add files via upload

---
+++ Naming.playground/Pages/12-ExerciseMorning.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+/*: 
+ ## Exercise: Fixing Your Morning
+ 
+ There’s a lot to get done before you leave home in the morning. This exercise will help you optimize your routine.
+ 
+ - callout(Exercise): Create a constant for each activity you do in the morning before leaving home: things like `brushTeeth`, `uploadPhotos`, `chooseClothes`, `shower`, `goJogging`, `finishHomework`, `fixLunch`, and so on. Think about how many minutes each activity usually takes, and assign that value to each constant.
+ */
+let brushTeeth = 3
+// Add more here...
+let chooseClothes = 1
+
+let getWater = 2
+
+let fullyWakingUp = 10
+
+let PuttingOnClothes = 5
+
+let WaitingToLeaveforbus = 30
+//ususally I just play mobile games
+
+//: - callout(Exercise): Find the total time of all the activities by adding up the constants. Try to adjust the values or add more activities until the total time looks reasonably close to the actual amount of time you spend getting ready on an average day.
+let TotalTimetoLeave = brushTeeth + chooseClothes + getWater + fullyWakingUp + PuttingOnClothes + WaitingToLeaveforbus
+
+
+//: - callout(Exercise): Add up the constants again, but this time in separate groups: one group for things you have to do and another group for things you like to do.\
+//:\
+//:If there are things you don’t have to do and don't like to do, make a third group and go ahead and sum that one, too. \
+//:\
+//:Make a new constant for each group.
+let FunMinutesWhileWaiting = WaitingToLeaveforbus + fullyWakingUp
+
+let ThingsIHavetodo = brushTeeth + chooseClothes + getWater + PuttingOnClothes
+
+/*:
+ See what happens to your total time spent getting ready if you tweak the durations of the different activities. How short of a shower would you have to take in order to have more time to message your friends? Or go for a longer run? How much more time would you need if you decided to spend as long as you wanted doing all the activities you like best?
+ 
+ Change the numbers until you’ve got a design for your ideal morning. What would have to change in order for you to be able to actually spend your morning time this way?
+*/
+let FullyWakingUp = 30
+
+let brushteeth = 3
+
+let waitingtoLeaveforbus = 45
+
+let drinkwater = 1
+
+let putclotheson = 2
+//i'll have my clothes ready and quick to put on plus i would just wake up earlier.
---
+++ Strings.playground/Pages/Exercise-Making a List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+/*:
+ ## Exercise: Making a List
+ 
+ Lists are great. Here are some constants describing some of the things you’ve learned about strings so far:
+ */
+let constants = "Declaring string constants"
+let unicode = "Unicode characters (😎)"
+let combining = "Combining strings using +"
+let interpolation = "String interpolation (aka Fill in the Blanks)"
+let escaping = "Escape characters for \"special powers\""
+let newline = "Making new lines"
+/*:
+ - experiment: Make a new string constant that is a list of the things you’ve learned, with each entry on a new line. Make sure you add the result to the playground page so that you can see the list properly.
+ */
+let Variables = "learning variables "
+let string = "creating strings linking text"
+let characters = "each character takes up space, even invisible ones"
+let math = "swift is like a calculator and automaticly does math by how you put it in"
+let constant = " utilizing the constant feature"
+let decode = "realizing how to understand code from what is given"
+//:
+//:[Previous](@previous)  |  page 13 of 16  |  [Next: Exercise: A Restaurant](@next)
